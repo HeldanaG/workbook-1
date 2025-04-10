bf65efac30027ba17bc5f63bd1a92757dce4abc0
HeldanaG <heldanagmariam@gmail.com>
2025-04-10 11:25:24 -0500

    completed shoppinglist app

---
+++ shopping-list/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+        System.out.println("\nYour shopping list should include these items:");
+        System.out.println("1. Milk");
+        System.out.println("2. Egg");
+        System.out.println("3. Bread");
+        System.out.println("4. Cheese");
+        System.out.println("5. Yogurt");
+        System.out.println("6. Onion");
+        System.out.println("7. Tomato");
+        System.out.println("8. Oil");
+        System.out.println("9. Potato");
+        System.out.println("10. Garlic");
+
+    }
+}
