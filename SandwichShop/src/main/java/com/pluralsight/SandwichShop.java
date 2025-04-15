d6e7da5583b214f99144b172f913535656a69d33
HeldanaG <heldanagmariam@gmail.com>
2025-04-15 11:04:35 -0500

    Added Loaded option for the sandwich shop

--- SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
+++ SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
@@ -6,20 +6,19 @@ public class SandwichShop {
     public static void main(String[] args) {
 
         System.out.println("\n1:Regular: base price $5.45 \n2:Large: base price $8.95");
-        System.out.print("Which type of sandwich you want:");
+        System.out.print("Which type of sandwich option you want:");
         int option = input.nextInt();
         double sandwichPrice=choice(option);
 
         input.nextLine();
         System.out.print("\nDo you want to do loaded sandwich(double everything):");
-        String loadedChoice= input.nextLine();
+        String loadedChoice= input.nextLine().toLowerCase();
         if(loadedChoice.equals("yes") && option ==1){
             sandwichPrice+=1;
         }
         else if (loadedChoice.equals("yes") && option ==2){
             sandwichPrice+=1.75;
         }
-
 
         System.out.println("\nStudent (17 old or younger) receive a 10% discount");
         System.out.println("Senior (65 old or older) receive a 20% discount");
@@ -28,7 +27,6 @@ public class SandwichShop {
 
         double discountAmount=discount(age,sandwichPrice);
         double finalPrice=sandwichPrice-discountAmount;
-
         System.out.printf("\nThe cost of the sandwich is $%.2f", finalPrice);
 
     }
